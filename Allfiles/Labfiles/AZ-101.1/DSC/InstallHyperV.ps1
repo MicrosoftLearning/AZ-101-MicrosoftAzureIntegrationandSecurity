e67a4d58eae95d1cc9728cb5b4fa0bdb625c59ba
polichtm <polichtm@hotmail.com>
2019-02-28 07:34:53 -0800

    Update InstallHyperV.ps1

--- Allfiles/Labfiles/AZ-101.1/DSC/InstallHyperV.ps1
+++ Allfiles/Labfiles/AZ-101.1/DSC/InstallHyperV.ps1
@@ -19,13 +19,6 @@ configuration InstallHyperV
             IncludeAllSubFeature = $true
         }
 
-        WindowsFeature HyperV-Manager
-        { 
-            Ensure = "Present" 
-            Name = "Microsoft-Hyper-V-Tools-All"
-            IncludeAllSubFeature = $true
-        }
-
         LocalConfigurationManager 
         {
             ConfigurationMode = 'ApplyOnly'
